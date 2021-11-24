e92222833682359512886bc5071dee08e4dfa269
Emkay16 <mabelkohjx@gmail.com>
2021-11-24 16:48:32 +0800

    Update colour theme

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import './Header.css';
+
+function Header() {
+  return(
+    <div className="header">
+
+      <div classname="logo">MABEL</div>
+      
+      <div classname="nav-buttons">
+
+        <div classname="button">
+          button 1
+        </div>
+        
+        <div classname="button">
+          button 2
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header;
